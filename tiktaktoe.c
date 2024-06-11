5234f7e6845b4ebbd044784d9f29f2cefd031f5e
Ayush Soni <98144432+soniayush12@users.noreply.github.com>
2024-06-11 19:50:03 +0530

    Update tiktaktoe.c

--- tiktaktoe.c
+++ tiktaktoe.c
@@ -1,95 +1,89 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-char board[3][3] = {
-    {'-', '-', '-'},
-    {'-', '-', '-'},
-    {'-', '-', '-'}
-};
+char board[3][3] = { {'-', '-', '-'},
+                     {'-', '-', '-'},
+                     {'-', '-', '-'} };
 
 int gameover = 0;
 int currentPlayer = 1;
 
-void drawBoard() {
-    printf("\n\n");
+void draw() {
+    system("cls");
+    printf("\n\n\n\n\n\n\t\t\t\t\t##########################\n");
     for (int i = 0; i < 3; i++) {
-        printf("\t\t\t\t\t");
+        printf("\t\t\t\t\t##      ##      ##      ##\n");
         for (int j = 0; j < 3; j++) {
-            printf("%c ", board[i][j]);
+            printf("\t\t\t\t\t##   %c  ", board[i][j]);
         }
-        printf("\n");
+        printf("##\n");
+        printf("\t\t\t\t\t##      ##      ##      ##\n");
+        printf("\t\t\t\t\t##########################\n");
     }
-    printf("\n");
+    printf("\n\t\t\t\t\tPLAYER 1 = X    PLAYER 2 = O \n");
 }
 
-void playerMove() {
+void player() {
     int row, col;
-    printf("\n\nPlayer %d's turn\n", currentPlayer);
-    printf("Enter the row and column (1-3): ");
-    scanf("%d %d", &row, &col);
-    row--;
-    col--;
-
+    printf("\n\n\n\t\t\t\t############################################\n");
+    printf("\t\t\t\t#            Player %d turn                 #\n", currentPlayer);
+    printf("\t\t\t\t#                                          #\n");
+    printf("\t\t\t\t#     Enter the row you want to mark       #\n");
+    printf("\t\t\t\t#               row = ");
+    scanf("%d", &row);
+    printf("\t\t\t\t#                                          #\n");
+    printf("\t\t\t\t#      Enter the row you want to mark      #\n");
+    printf("\t\t\t\t#               col = ");
+    scanf("%d", &col);
+    printf("\t\t\t\t#                                          #\n");
+    printf("\t\t\t\t############################################");
+    row--; col--;
     if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == '-') {
         if (currentPlayer == 1)
             board[row][col] = 'X';
         else
             board[row][col] = 'O';
-    } else {
-        printf("Invalid move! Try again.\n");
-        playerMove();
+        currentPlayer = (currentPlayer == 1) ? 2 : 1;
+    }
+    else {
+        printf("\n\t\t\t\t ERROR = Invalid Choice.....\n\t\t\t\t Press any INTEGER to resume..");
+        int er;
+        scanf("%d", &er);
     }
 }
 
-int checkWin() {
-    // Check rows and columns
-    for (int i = 0; i < 3; i++) {
-        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != '-')
-            return 1;
-        if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != '-')
-            return 1;
-    }
-
-    // Check diagonals
-    if ((board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '-') ||
-        (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != '-'))
-        return 1;
-
-    // Check for draw
-    int draw = 1;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (board[i][j] == '-') {
-                draw = 0;
-                break;
-            }
-        }
-    }
-    if (draw) return 2;
-
-    return 0;
-}
-
-void togglePlayer() {
-    currentPlayer = (currentPlayer == 1) ? 2 : 1;
+void checkWinner() {
+    if (board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] != '-')
+        gameover = 1;
+    else if (board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] != '-')
+        gameover = 1;
+    else if (board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] != '-')
+        gameover = 1;
+    else if (board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[0][0] != '-')
+        gameover = 1;
+    else if (board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[0][1] != '-')
+        gameover = 1;
+    else if (board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[0][2] != '-')
+        gameover = 1;
+    else if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '-')
+        gameover = 1;
+    else if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != '-')
+        gameover = 1;
 }
 
 int main() {
     while (!gameover) {
-        drawBoard();
-        playerMove();
-        int result = checkWin();
-        if (result == 1) {
-            drawBoard();
-            printf("Player %d wins!\n", currentPlayer);
-            gameover = 1;
-        } else if (result == 2) {
-            drawBoard();
-            printf("It's a draw!\n");
-            gameover = 1;
-        } else {
-            togglePlayer();
-        }
+        draw();
+        player();
+        checkWinner();
     }
+    draw();
+    printf("\n\n\n\t\t\t\t############################################\n");
+    printf("\t\t\t\t##           CONGRATULATIONS !!           ##\n");
+    printf("\t\t\t\t##                                        ##\n");
+    printf("\t\t\t\t##         PLAYER %d WON THE MATCH         ##\n", currentPlayer);
+    printf("\t\t\t\t##                                        ##\n");
+    printf("\t\t\t\t############################################\n");
 
     return 0;
 }
